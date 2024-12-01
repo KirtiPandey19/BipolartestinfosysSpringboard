a9df9ace4df23f42c044d8a221287db5d19efb08
Kirti Pandey <114803597+KirtiPandey19@users.noreply.github.com>
2024-12-01 20:18:26 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BipolarTest from "./BipolarTest"; // Import the BipolarTest component
+
+function App() {
+  return (
+    <div>
+      <BipolarTest /> {/* Render the BipolarTest component */}
+    </div>
+  );
+}
+
+export default App;
